963e29596b60e25b142580ebe7087d91483ab278
sbendl <sbendl@uncc.edu>
2017-08-25 15:18:13 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main() {
+    int NUM_BODIES = 1000;
+    double MIN_SPEED = 100;
+    double MAX_SPEED = 50000;
+    double GRAV_CONST = 6.67408 * pow(10, -11);
+    int BASE_PART_MASS = 1000000;
+
+    // Set up initial position and velocity
+
+    for (int i = 0 ; i < )
+
+}
